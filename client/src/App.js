942755cd4dc3c6d1ee537843b18759c58d367014
TalDahan2304 <tald234097@gmail.com>
2023-01-21 16:35:48 +0200

    receiving data with socket.io

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client';
+const socket= io.connect("http://localhost:3001");
 
 function App() {
+  const sendMessage= () =>{
+    socket.emit("send_message", { message: "Hello" });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input placeholder="Message..."/>
+      <button onClick={sendMessage}>Send Message</button>
     </div>
   );
 }
